be6c9e60edf626eb73623bcd69e2bb8eeef14c0b
rrahul0903 <rahul.ranade@qed42.com>
2020-05-30 12:15:06 +0530

    First commit......

---
+++ src/mypackage/Test1.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if (hungry=="yes")
+			System.out.println("Eat Something");
+		else
+		{
+			System.out.println("Do work");
+		}
+
+	}
+
+}
